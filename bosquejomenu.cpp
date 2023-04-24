620369c9a77a345914ccc4d5843e99488e12e9eb
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-24 16:49:00 -0500

    Crear bosquejo del menu

---
+++ bosquejomenu.cpp
@@ -0,0 +1,15 @@
+void menu(){
+    /*
+     * 1. Calcular ruta más cercana entre dos nodos
+     * 2. Mostrar Camino para obtener dicha ruta
+     * 3. Agregar Nodo
+     *  3.1. Establecer conexion
+     *  3.2. Peso por cada coenxion
+     * Nota: Cuando establece conexion se debe actualizar el mapa del nodo escogido para conectar
+     * es decir. Inicialmente 'A' => {'B':10, 'D': 20}
+     * NUEVO NODO => 'X' Conectar con 'A'
+     *              'X' => {'A':33}
+     *              'A' => {'B':10, 'D': 20,'X':33}
+     * 4. Eliminar nodo
+     */
+}
